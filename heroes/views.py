42550b8c210ff9f66cb9f6038c296a2397a47fba
ed_gomes <edgargomesoliveira448@gmail.com>
2025-08-27 14:35:52 -0300

    Primeira views e rota de template

--- heroes/views.py
+++ heroes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Hero
+
+
+def lista_herois(request):
+    herois = Hero.objects.all()  # busca todos os heróis do banco
+    return render(request, "heroes/lista_herois.html", {"herois": herois})
 
 # Create your views here.
