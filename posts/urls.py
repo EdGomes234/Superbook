bfffb1a462fbae1cb1b43b23b8112718279c3668
ed_gomes <edgargomesoliveira448@gmail.com>
2025-09-17 14:22:25 -0300

    Teste Finalizado

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,12 @@
 # EDGAR GOMES OLIVEIRA
 
 from django.urls import path
-from .views import PostCreateView, PostListView, PostUpdateView, PostDeleteView
+from .views import PostCreateView, PostListView, PostUpdateView, PostDeleteView, detalhe_post
 
 urlpatterns = [
     path('novo/', PostCreateView.as_view(), name='novo_post'),
     path('lista/', PostListView.as_view(), name='lista_posts'),
     path('<int:pk>/editar/', PostUpdateView.as_view(), name='editar_post'),
     path('<int:pk>/excluir/', PostDeleteView.as_view(), name='excluir_post'),
+    path('<int:pk>/', detalhe_post, name='detalhe_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,10 @@ from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
+from comments.forms import ComentarioForm
 
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
 
 # Create your views here.
 class PostCreateView(CreateView):
@@ -27,3 +30,23 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'posts/confirmar_exclusao.html'
     success_url = reverse_lazy('lista_posts')
+
+def detalhe_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comentarios = post.comentarios.all()
+
+    if request.method == 'POST':
+        form = ComentarioForm(request.POST)
+        if form.is_valid():
+            comentario = form.save(commit=False)
+            comentario.post = post
+            comentario.save()
+            return redirect('detalhe_post', pk=post.pk)
+    else:
+        form = ComentarioForm()
+
+    return render(request, 'posts/detalhe_post.html', {
+        'post': post,
+        'comentarios': comentarios,
+        'form': form
+    })
